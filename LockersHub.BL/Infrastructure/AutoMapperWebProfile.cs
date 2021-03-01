09b3a2cb31e1429a6710bca375be2934cb77b490
Aduwu Joseph <aduwujoseph4real@gmail.com>
2021-03-01 19:44:28 +0100

    -----I created an automapper registry class where i registered the models that i want to auto mapped. Also, I created a dependency class where i extend the IserviceCollection and add the Business logic (BL) classes to the scope.----

---
+++ LockersHub.BL/Infrastructure/AutoMapperWebProfile.cs
@@ -0,0 +1,47 @@
+﻿using LockersHub.BL.DVM;
+using LockersHub.DALS.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace LockersHub.BL.Infrastructure
+{
+	public class AutoMapperWebProfile : AutoMapper.Profile
+	{
+		public AutoMapperWebProfile()
+        {
+
+            CreateMap<AspNetRoleClaim, AspNetRoleClaimDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<AspNetRoleClaimDVM, AspNetRoleClaim>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<AspNetRole, AspNetRoleDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<AspNetRoleDVM, AspNetRole>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<AspNetUserClaim, AspNetUserClaimDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<AspNetUserClaimDVM, AspNetUserClaim>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<AspNetUser, AspNetUserDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<AspNetUserDVM, AspNetUser>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<AspNetUserLogin, AspNetUserLoginDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<AspNetUserLoginDVM, AspNetUserLogin>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<AspNetUserRole, AspNetUserRoleDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<AspNetUserRoleDVM, AspNetUserRole>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<AspNetUserToken, AspNetUserTokenDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<AspNetUserTokenDVM, AspNetUserToken>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<City, CityDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<CityDVM, City>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<Country, CountryDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<CountryDVM, Country>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<Locker, LockerDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<LockerDVM, Locker>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<State, StateDVM>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            CreateMap<StateDVM, State>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+        }
+    }
+}
---
+++ LockersHub.BL/Infrastructure/IServiceCollectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using LockersHub.BL.BusinessLogic;
+using LockersHub.BL.BusinessLogic.Interfaces;
+using LockersHub.DALS.Infrastructure;
+using LockersHub.DALS.Infrastructure.Interfaces;
+using LockersHub.DALS.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockersHub.BL.Infrastructure
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddBLLDependenciesLibraries(this IServiceCollection services)
+        {
+
+            services.AddEntityFrameworkSqlServer();
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AutoMapperWebProfile());
+            });
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ILockersBL, LockersBL>();
+            services.AddScoped<ISetupsBL, SetupsBL>();
+            services.AddScoped<IUsersBL, UsersBL>();
+
+            return services;
+        }
+    }
+}
