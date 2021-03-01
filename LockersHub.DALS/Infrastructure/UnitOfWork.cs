c42fee12e2d3f2f640650ace9c4e93c003ebc901
Aduwu Joseph <aduwujoseph4real@gmail.com>
2021-03-01 07:16:29 +0100

    Added generic repository classes to use with EntityFrameworkCore to communnicate with my database

---
+++ LockersHub.DALS/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using LockersHub.DALS.Entities;
+using LockersHub.DALS.Infrastructure.Interfaces;
+
+namespace LockersHub.DALS.Infrastructure
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		/// <summary>
+		/// This readonly variable creates the context of the database
+		/// </summary>
+		private readonly LockersDBContext _dbContext;
+
+		/// <summary>
+		/// This constructor initializes the database
+		/// </summary>
+		public UnitOfWork(LockersDBContext context)
+		{
+			_dbContext = context;
+		}
+
+		/// <summary>
+		/// This method Returns the db context created from the constructor
+		/// </summary>
+		public DbContext Db
+		{
+			get { return _dbContext; }
+		}
+
+		private bool disposed = false;
+		/// <summary>
+		/// This method disposes the database
+		/// </summary>
+		public virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					_dbContext.Dispose();
+				}
+			}
+
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
