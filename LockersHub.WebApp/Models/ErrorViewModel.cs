90d4cf923e217006a4f246e006a51a7d29f967f2
Aduwu Joseph <aduwujoseph4real@gmail.com>
2021-03-04 13:27:17 +0100

    ---Created the UI to consume the API using Asp.Net Core 3.1 MVC---

---
+++ LockersHub.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LockersHub.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
